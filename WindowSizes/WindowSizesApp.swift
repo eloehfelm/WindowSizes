5ed68feaabc9a166009288b3cb644ebf58a536e7
erik <eloehfelm@mac.com>
2024-08-02 13:48:26 -0400

    Initial Commit

---
+++ WindowSizes/WindowSizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSizesApp.swift
+//  WindowSizes
+//
+//  Created by Erik Loehfelm on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WindowSizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WindowSizesTests/WindowSizesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSizesTests.swift
+//  WindowSizesTests
+//
+//  Created by Erik Loehfelm on 8/2/24.
+//
+
+import Testing
+@testable import WindowSizes
+
+struct WindowSizesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
